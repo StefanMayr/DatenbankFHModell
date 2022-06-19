6715da03792ecfa0d018d14ae1141cc5335ecdef
Stefan Mayr <S2010438017@fhooe.at>
2022-06-19 15:22:51 +0200

    Push

--- DatenbankFHModell/Entities/DBManager.cs
+++ DatenbankFHModell/Entities/DBManager.cs
@@ -202,7 +202,7 @@ namespace DatenbankFHModell
         }
 
         /// <summary>
-        /// Tested and ok
+        /// Push Student into Db
         /// </summary>
         /// <param name="Matrikelnummer"></param>
         /// <param name="Name"></param>
@@ -214,6 +214,105 @@ namespace DatenbankFHModell
             string query = "INSERT INTO Student(Matrikelnummer,NameStudent,WohnortStudent,GeschlechtStudent,AlterStudent) VALUES (" + Matrikelnummer + ",'" + Name + "','" + Wohnort + "','" + Geschlecht + "'," + Alter + ");";
             Insert(query);
         }
-    
-}
+
+        /// <summary>
+        /// Push Lehrender into DB
+        /// </summary>
+        /// <param name="Personalnummer"></param>
+        /// <param name="Name"></param>
+        /// <param name="Wohnort"></param>
+        /// <param name="Alter"></param>
+        /// <param name="Ausbildung"></param>
+        /// <param name="Geschlecht"></param>
+        /// <param name="Fakultätnummer"></param>
+        public void PushLehrender(int Personalnummer, string Name, string Wohnort, int Alter, string Ausbildung, string Geschlecht, int Fakultätnummer)
+        {
+            string query = "INSERT INTO Student(Personalnummer,NameLehrender,WohnortLehrender,AlterLehrender,AusbildungLehrender, GeschlechtLehrender, Fakultät_Fakultätnummer) VALUES (" + Personalnummer + ",'" + Name + "','" + Wohnort + "'," + Alter + ",'" + Ausbildung + "','" + Geschlecht + "'," + Fakultätnummer + ");";
+            Insert(query);
+        }
+
+        /// <summary>
+        /// Push Lehrverantalung into DB
+        /// </summary>
+        /// <param name="Lehrveranstaltungsnummer"></param>
+        /// <param name="NameLehrveranstaltung"></param>
+        /// <param name="Datum"></param>
+        /// <param name="Einheit"></param>
+        public void PushLehrveranstaltung(int Lehrveranstaltungsnummer,string NameLehrveranstaltung, DateTime Datum, int Einheit)
+        {
+            string query = "INSERT INTO Lehrveranstaltung(Lehrverantaltungsnummer,NameLehrveranstaltung,DatumLehrveranstaltung,Einheit) VALUES (" + Lehrveranstaltungsnummer + ",'" + NameLehrveranstaltung + "'," + Einheit + ");";
+            Insert(query);
+        }
+
+        /// <summary>
+        /// Push Gebaeude into DB
+        /// </summary>
+        /// <param name="Lehrveranstaltungsnummer"></param>
+        /// <param name="NameLehrveranstaltung"></param>
+        /// <param name="Datum"></param>
+        /// <param name="Einheit"></param>
+        public void PushGebaeude(int Gebauedenummer)
+        {
+            string query = "INSERT INTO Gebaeude(Gebaeudenummer) VALUES (" + Gebauedenummer + ");";
+            Insert(query);
+        }
+
+        /// <summary>
+        /// Push Fakultaet into DB
+        /// </summary>
+        /// <param name="Fakultaetnummer"></param>
+        /// <param name="NameFakultaet"></param>
+        public void PushFakultaet(int Personalnummer, int Fakultaetnummer, int Lehrveranstaltungsnummer)
+        {
+            string query = "INSERT INTO Fakultät(Fakultätnummer,NameFakultät) VALUES (" + Fakultaetnummer + ",'" + NameFakultaet + "');";
+            Insert(query);
+        }
+
+        /// <summary>
+        /// Push into Lehrender has Lehrveranstaltung 
+        /// </summary>
+        /// <param name="Fakultaetnummer"></param>
+        /// <param name="NameFakultaet"></param>
+        public void PushLehrendehatLehrveranstaltung(int Fakultaetnummer, string NameFakultaet)
+        {
+            string query = "INSERT INTO Lehrende_has_Lehrveranstaltung(Personalnummer,Fakultätnummer,Lehrveranstaltungsnummer) VALUES (" + Personalnummer + "," + Fakultaetnummer + "," + Lehrveranstaltungsnummer + ");";
+            Insert(query);
+        }
+
+        /// <summary>
+        /// Push Lehrveranstaltung_has_Room into DB
+        /// </summary>
+        /// <param name="Lehrveranstaltungsnummer"></param>
+        /// <param name="Raumnummer"></param>
+        /// <param name="Gebaeudenummer"></param>
+        public void PushLehrveranstaltunghatRaum(int Lehrveranstaltungsnummer, int Raumnummer, int Gebaeudenummer)
+        {
+            string query = "INSERT INTO Lehrveranstaltung_has_raum(Lehrveranstaltungsnummer,Raumnummer,Gebaeudenummer) VALUES (" + Lehrveranstaltungsnummer + "," + Raumnummer + "," + Gebaeudenummer + ");";
+            Insert(query);
+        }
+
+        /// <summary>
+        /// Push Raum into DB
+        /// </summary>
+        /// <param name="Raumnummer"></param>
+        /// <param name="Gebaeudenummer"></param>
+        public void PushRaum(int Raumnummer, int Gebaeudenummer)
+        {
+            string query = "INSERT INTO raum(Raumnummer,Gebaeudenummer) VALUES (" + Raumnummer + "," + Gebaeudenummer + ");";
+            Insert(query);
+        }
+
+        /// <summary>
+        /// Push StudenthatLehrveranstaltung into DB
+        /// </summary>
+        /// <param name="Matrikelnummer"></param>
+        /// <param name="Lehrveranstaltungsnummer"></param>
+        public void PushStudenthatLehrverantaltung(int Matrikelnummer, int Lehrveranstaltungsnummer)
+        {
+            string query = "INSERT INTO Student_has_lehrveranstaltung(Matrikelnummer,Lehrveranstaltungsnummer) VALUES (" + Matrikelnummer + "," + Lehrveranstaltungsnummer + ");";
+            Insert(query);
+        }
+
+
+    }
 }
