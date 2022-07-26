e8ba0168e810e6ba472eee3cdae3aa052a3458f2
chris <chris@DESKTOP-VC5VPK8>
2022-07-26 14:38:10 +0200

    add

--- DatenbankFHModell/View/Lehrveranstaltung.xaml.cs
+++ DatenbankFHModell/View/Lehrveranstaltung.xaml.cs
@@ -31,7 +31,7 @@ namespace DatenbankFHModell
 
         private void btn_Insert_Click(object sender, RoutedEventArgs e)
         {
-            TheDBManager.PushLehrender(Convert.ToInt32(txt_Pers.Text), txt_Name.Text, txt_Wohnort.Text, Convert.ToInt32(txt_Alter.Text), txt_Ausbildung.Text, txt_Geschl.Text, Convert.ToInt32(txt_Fakultät.Text));
+            TheDBManager.PushLehrveranstaltung(Convert.ToInt32(txt_Pers.Text), txt_Name.Text, txt_Wohnort.Text, Convert.ToInt32(txt_Alter.Text), txt_Ausbildung.Text, txt_Geschl.Text, Convert.ToInt32(txt_Fakultät.Text));
         }
 
         private void btn_Close_Click(object sender, RoutedEventArgs e)
